2e821fe4953779bbd3e886810a29051cb5734401
https://github.com/muhammetzinnet/Veriable <maicek@hotmail.com>
2022-09-16 23:44:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ogrenciSayisi = 11;
+        String mesaj = "Öğrenci sayısı: ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("ğrenci sayısı: " + ogrenciSayisi);
+        System.out.println("ğrenci sayısı: " + ogrenciSayisi);
+        System.out.println("ğrenci sayısı: " + ogrenciSayisi);
+    }
+}
